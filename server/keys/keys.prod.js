494aa3d724cc5f11cb7596e0883b9bf9696abb6d
Alexander272 <martynovalexander272@gmail.com>
2021-02-23 11:04:28 +0500

    fix keys

--- server/keys/keys.prod.js
+++ server/keys/keys.prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     MONGODB_URL: process.env.MONGODB_URL,
     SESSION_SECRET: process.env.SESSION_SECRET,
-    BASE_URL: prosses.env.BASE_URL,
+    BASE_URL: process.env.BASE_URL,
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-    EMAIL_FROM: prosses.env.EMAIL_FROM,
+    EMAIL_FROM: process.env.EMAIL_FROM,
 }
